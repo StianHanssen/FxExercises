9270d7dc6df70464f8ca9ddebafe5e2c20561008
Stian Hanssen <Hanssen.Stian@gmail.com>
2016-02-05 08:51:15 +0100

    Clean up

--- FxExercises/Exercise 1/fxmlCode/Main.java
+++ FxExercises/Exercise 1/fxmlCode/Main.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 public class Main extends Application {
 	@Override
     public void start(Stage stage) throws Exception {
-       Parent root = FXMLLoader.load(getClass().getResource("Runner.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("Runner.fxml"));
     
         Scene scene = new Scene(root, 390, 256);
     
